b94b1544a1399cd8bb8ffe992353a7575a6a8cc8
Soumya Kanti Ghosh <soumyaghosh.ghosh@gmail.com>
2016-12-22 18:21:34 +0530

    change

--- TestProject/public/app/app.js
+++ TestProject/public/app/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('employeeRecords', []).constant('API_URL', 'http://localhost:8000/angulara/public/api/v1/');
+var app = angular.module('employeeRecords', []).constant('API_URL', 'http://localhost:8000/api/v1/');
