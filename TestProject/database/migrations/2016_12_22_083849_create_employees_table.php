71195ea5b6315daf26b59d84a24512f96a38edc8
Soumya Kanti Ghosh <soumyaghosh.ghosh@gmail.com>
2016-12-22 14:16:03 +0530

    none

--- TestProject/database/migrations/2016_12_22_083849_create_employees_table.php
+++ TestProject/database/migrations/2016_12_22_083849_create_employees_table.php
@@ -14,6 +14,9 @@ class CreateEmployeesTable extends Migration
     public function up()
     {
         //
+        Schema::create('employees',function(Blueprint $table){
+            $table-
+        });
     }
 
     /**
